30097ebe12768743b00392d84fd7b4da7b4ffa85
Joseph David <jodavid444dave@gmail.com>
2023-03-29 18:03:35 +0100

    Modify queries.sql file

--- queries.sql
+++ queries.sql
@@ -3,8 +3,8 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered = 'true' AND escape_attempts < 3;
-SELECT date_of_birth  FROM * animals WHERE IN ('Agumon', 'Pikachu');
+SELECT date_of_birth  FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'true';
-SELECT * FROM annimals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
