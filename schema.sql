d48850fe73d43c2a785d0533cdb52de88b5bab57
Joseph David <jodavid444dave@gmail.com>
2023-04-01 08:50:29 +0100

    Modify species table

--- schema.sql
+++ schema.sql
@@ -33,3 +33,16 @@ DROP COLUMN species;
 
 ALTER TABLE animals 
 ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY(owner_id)
+REFERENCES owners(id);
