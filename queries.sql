75c9341beef6e0724a77c7d14b1558d32cd2393e
Joseph David <jodavid444dave@gmail.com>
2023-04-01 12:24:38 +0100

    Update queries .sql file to fix required changes

--- queries.sql
+++ queries.sql
@@ -165,10 +165,14 @@ SELECT animals.name AS "RECENT ANIMAL SEEN", vets.name AS "VET NAME", visit_date
     JOIN vets ON visits.vets_id = vets.id
     ORDER BY visit_date DESC LIMIT 1;
 
-SELECT COUNT(*) AS "number of visits" 
-  FROM vets INNER JOIN visits ON vets.id = visits.vets_id 
-  LEFT JOIN specializations ON vets.id = specializations.vet_id 
-  LEFT JOIN species ON specializations.species_id = species.id WHERE species IS NULL;
+SELECT COUNT(*)
+    FROM visits
+    JOIN animals ON visits.animals_id = animals.id
+    JOIN vets ON visits.vets_id = vets.id
+    WHERE animals.species_id NOT IN (
+    SELECT species_id FROM specializations
+	  WHERE specializations.vets_id = vets.id
+);
 
     SELECT species.name AS "Species Name" 
     FROM visits
