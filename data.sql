3c58577baaf4759f159b54aec2739550f402aaee
Joseph David <jodavid444dave@gmail.com>
2023-04-01 08:34:57 +0100

    Update owners table

--- data.sql
+++ data.sql
@@ -32,3 +32,13 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
  VALUES ('Ditto', '2022-05-14', 4, 'true', 22);
+
+
+-- Multiple tables
+INSERT INTO owners (full_name, age) 
+VALUES ('Sam Smith',34 ),
+       ('Jennifer Orwel',19 ),
+       ('Bob',45 ),
+       ('Melody Pond', 77),
+       ('Dean Winchester', 14),
+       ('Jodie Whittaker', 38);
