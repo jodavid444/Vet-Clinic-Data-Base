1501054bee648b539d02352f6029e84eb706671d
Joseph David <jodavid444dave@gmail.com>
2023-04-01 09:29:13 +0100

    Create a join table name visits to handle relationship

--- schema.sql
+++ schema.sql
@@ -63,3 +63,24 @@ CREATE TABLE specializations (
   CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
   CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
 );
+
+ALTER TABLE specializations 
+ADD CONSTRAINT PK_specializations 
+PRIMARY KEY (species_id ,vets_id);
+
+
+CREATE TABLE visits (
+  animal_id INT,
+  vets_id INT,
+  visit_date DATE,
+  CONSTRAINT fk_animal FOREIGN KEY(animal_id)REFERENCES animals(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id)REFERENCES vets(id),
+  CONSTRAINT PK_visits PRIMARY KEY (animal_id ,vets_id)
+);
+
+ALTER TABLE visits
+DROP CONSTRAINT PK_visits;
+
+ALTER TABLE visits 
+ADD CONSTRAINT PK_visits 
+PRIMARY KEY (animal_id, vets_id, visit_date);
