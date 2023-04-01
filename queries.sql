02713c001e3f2bb3aebe7af2f819660237d4653e
Joseph David <jodavid444dave@gmail.com>
2023-04-01 07:55:44 +0100

    Add query to check average number of escape attempts per animal type of those born between 1990 and 2000

--- queries.sql
+++ queries.sql
@@ -64,7 +64,12 @@ SELECT neutered, SUM(escape_attempts)
 FROM animals
 GROUP BY neutered;
 
-
 SELECT species, MIN(weight_kg),MAX(weight_kg)
 FROM animals
 GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
